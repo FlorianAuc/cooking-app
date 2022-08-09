2662e4aff63c97d77bcfce71caa687774db21f1f
BeCode <florian.aucouturier@gmail.com>
2022-08-09 16:05:44 +0200

    initial commit

---
+++ src/pages/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div className="cooking-container">
+      <h1>Cooking App</h1>
+      <input type="text" placeholder="Ex : beef" />
+    </div>
+  );
+};
+
+export default Home;
